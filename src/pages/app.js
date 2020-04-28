2883712db7b87b1b1be06d747a4bfb9e0b673523
swook <wook3024@gmail.com>
2020-04-28 09:50:10 +0900

    change axios baseURL from localhost to aws server adress

--- src/pages/app.js
+++ src/pages/app.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import Connection from "../route/front";
 import Navigation from "../components/navigation";
 
-axios.defaults.baseURL = "http://localhost:6060";
+axios.defaults.baseURL = "http://18.189.68.3:80";
 
 let store = createStore(reducer);
 
